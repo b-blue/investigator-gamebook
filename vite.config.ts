e177e673d2c7fa9372964540c0f7eaa50d1acc4b
B Blue <b@b-blue.dev>
2025-11-26 00:04:34 -0800

    fix: github pages path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/investigator-gamebook/',
   build: {
     outDir: 'docs',
   },
